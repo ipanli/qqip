c9d67f17a404f63707447cbaa8377c9e9fa7ba1c
zanjs <root@zanjs.com>
2015-12-16 10:10:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# qqip
+
+
+user qqip in Node.js
+
+http://lbs.qq.com/webservice_v1/guide-ip.html
+
+## install
+
+```bash
+npm i qqip
+```
+
+## usage
+
+```js
+qqip(function (err, ipinfo) {
+  console.log(ipinfo); 
+});
+```
---
+++ index.js
@@ -0,0 +1,16 @@
+var superagent = require('superagent');
+
+var getip = function (uip,ukey,callback) {
+  superagent
+    .get('http://apis.map.qq.com/ws/location/v1/ip?ip='+ uip +'8&key='+ukey)
+    .set('User-Agent', 'curl/7.37.1')
+    .end(function (err, res) {
+      if (err) {
+        return callback(err);
+      }
+      var ipinfo = res.text;
+      callback(null, ipinfo);
+    });
+};
+
+module.exports = exports = getip;
